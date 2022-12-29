f36c4e5708bf9279931acba8780fc97f2acc42a8
dino <kevin.dinocera@protonmail.com>
2022-12-29 23:19:58 +0100

    add html file

--- script.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_COOKIE['color-scheme'] == 'dark') {
-  echo '<img src="0XCAF3_day.png" id="profile-picture">';
-} else {
-  echo '<img src="0XCAF3_night.png" id="profile-picture">';
-}
-?>
-
-<?php include('https://your-username.github.io/your-repository/your-script.php'); ?>
