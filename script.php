45b12d8f96073941d21905c72163169ba4fd48f8
kdi-noce <92105776+0xCAF3D0OD@users.noreply.github.com>
2022-12-29 20:06:43 +0100

    Create script.php

---
+++ script.php
@@ -0,0 +1,9 @@
+<?php
+if ($_COOKIE['color-scheme'] == 'dark') {
+  echo '<img src="night-theme-picture.jpg" id="profile-picture">';
+} else {
+  echo '<img src="day-theme-picture.jpg" id="profile-picture">';
+}
+?>
+
+<iframe src="https://your-username.github.io/your-repository/your-script.php" id="profile-picture-frame"></iframe>
